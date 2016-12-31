f2893bb9bb91c6fb6483c62c23e102377ca8a551
kry496 <kry496@my.utsa.edu>
2016-12-30 20:53:52 -0600

    update .01

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,23 @@
 from os import system
 from platform import platform
 from fabric.api import settings
-from fabric.context_managers import  hide
+from fabric.context_managers import  hide, quiet
 
 def java_version():
-	with settings(hide('user')):
-		if system("which java") >= 1:
-			if 'ubuntu' in platform().lower:
-				system(" sudo apt-get -y install default-jdk")
-			elif 'centos' in platform().lower:
-				system(" sudo yum install -y default-jdk")
-			else:
-				print "this script works only on ubuntu or centos linux distribution"
+#	with settings(hide('user', 'stdout')):
+	with quiet():
+		_java_fun()
+
+
+def _java_fun():
+	if system("which java") >= 1:
+		if 'ubuntu' in platform().lower:
+			system(" sudo apt-get -y install default-jdk")
+		elif 'centos' in platform().lower:
+			system(" sudo yum install -y default-jdk")
 		else:
-			print "java is already installed"
+			print "this script works only on ubuntu or centos linux distribution"
+	else:
+		print "java is already installed"
 
 
